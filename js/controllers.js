4a99f818810343dacaa56718885e4ee50b7210bd
Mohit Kumar Mohit Kumar <mmohitku@isimtl.local>
2020-07-15 14:49:41 -0400

    contact form update ajax

--- js/controllers.js
+++ js/controllers.js
@@ -13,7 +13,7 @@ app.controller('ContactController', function ($scope, $http) {
         if (contactform.$valid) {
             $http({
                 method  : 'POST',
-                url     : 'contact-form.php',
+                url     : 'https://formspree.io/xoqkpnrw',
                 data    : $.param($scope.formData),  //param method from jQuery
                 headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
             }).success(function(data){
